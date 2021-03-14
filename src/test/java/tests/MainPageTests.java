87ae2e16229f6ec87b2c7cf8723197c92ba01b1e
roman.lelikov <roman.lelikov@brandmaker.com>
2021-03-14 15:50:19 +0700

    MainPageTests added @BeforeEach annotation

--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,11 @@ import static io.qameta.allure.Allure.step;
 
 @Tag("web")
 public class MainPageTests extends TestBase {
+
+    @BeforeEach
+    public void openMainPage() {
+        open("");
+    }
 
     @Test
     @DisplayName("Page should have product categories in the header")
