acb27235a9ca3ab784c2d1783545bf44b5def4d2
roman.lelikov <roman.lelikov@brandmaker.com>
2021-03-14 17:57:59 +0700

    added selenoid

--- src/test/java/settings/TestBase.java
+++ src/test/java/settings/TestBase.java
@@ -2,8 +2,15 @@ package settings;
 
 import com.codeborne.selenide.Configuration;
 import config.WebDriverConfig;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static settings.AttachmentsHelper.*;
 
 public class TestBase {
 
@@ -11,14 +18,31 @@ public class TestBase {
 
     @BeforeAll
     public static void settings() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
         Configuration.baseUrl = "https://shop.kalashnikov.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browser = config.getBrowserName();
         Configuration.browserVersion = config.getBrowserVersion();
 
         if (config.getRemoteURL() != null) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
             Configuration.remote = config.getRemoteURL();
         }
+    }
+
+    @AfterEach
+    public void attachments() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachText("Browser console logs", getConsoleLogs());
+        if (config.getRemoteURL() != null) {
+            attachVideo();
+        }
+        closeWebDriver();
     }
 }
 
