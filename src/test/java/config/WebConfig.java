1260a4d089a3dd533cf4cce94c3da6c06fa83055
roman.lelikov <roman.lelikov@brandmaker.com>
2021-03-18 18:07:01 +0700

    changed WebConfig.java

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/web.properties"
+        "classpath:web.properties"
 })
 public interface WebConfig extends Config {
     @Key("web.remote.driver.user")
