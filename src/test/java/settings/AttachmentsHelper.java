d0030255362cbf59084f6799a8a2fb3be3f9ff9e
roman.lelikov <roman.lelikov@brandmaker.com>
2021-04-01 20:55:44 +0700

    changed getSessionId

--- src/test/java/settings/AttachmentsHelper.java
+++ src/test/java/settings/AttachmentsHelper.java
@@ -52,7 +52,7 @@ public class AttachmentsHelper {
     }
 
     public static String getSessionId() {
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString().replace("selenoid", "");
     }
 
     public static String getConsoleLogs() {
